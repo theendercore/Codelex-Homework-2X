c71bb10a663a8e12c6e2e6ca2e82cd7a3010fed0
theendercore <theendercore@gmail.com>
2023-03-16 20:24:41 +0200

    - added icons - added search bar

---
+++ 28-homework/src/components/SearchBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import SearchIcon from "./icons/SearchIcon.vue"
+</script>
+
+<template>
+  <form class="">
+    <input type="text" name="city" id="city" class="rounded p-1" />
+    <button><SearchIcon class="w-[20px]" /></button>
+  </form>
+</template>
--- 28-homework/src/views/HomeView.vue
+++ 28-homework/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import WeatherItem from "@/components/WeatherItem.vue"
 import { useWeather } from "@/stores/weather"
+import SearchBar from "@/components/SearchBar.vue"
 
 const store = useWeather()
 </script>
@@ -8,17 +9,18 @@ const store = useWeather()
 <template>
   <main class="container m-auto flex flex-col items-center justify-center gap-4 p-20">
     <div class="btn-menu flex gap-10 rounded-full bg-slate-100 py-2 px-8">
-      <button @click="store.fetchWeather()" class="w-max rounded bg-gray-200 p-2">
+      <button @click="store.fetchWeather()" class="w-max rounded-xl bg-gray-200 p-2">
         Populate Info
       </button>
 
-      
       <button
         @click="store.toggleUnits()"
         class="rounded-full bg-gray-200 p-[.85rem] py-1 font-mono text-xl font-bold hover:bg-gray-300"
       >
         {{ store.searchParams.get("units") === "M" ? "C" : "F" }}
       </button>
+
+      <SearchBar />
     </div>
 
     <WeatherItem v-if="!store.error && JSON.stringify(store.data) !== '{}'" />
