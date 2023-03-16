c38ac14f213c30e78d4414e3190a044a742d3a67
theendercore <theendercore@gmail.com>
2023-03-16 20:34:57 +0200

    made the search functional

--- 28-homework/src/components/SearchBar.vue
+++ 28-homework/src/components/SearchBar.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
+import { useWeather } from "@/stores/weather"
+import { ref } from "vue"
 import SearchIcon from "./icons/SearchIcon.vue"
+
+let city = ref<string>("")
+let store = useWeather()
+
+function submit(e: Event) {
+  e.preventDefault()
+  store.searchParams.set("city", city.value)
+  city.value = ""
+  store.fetchWeather()
+}
 </script>
 
 <template>
-  <form class="">
-    <input type="text" name="city" id="city" class="rounded p-1" />
-    <button><SearchIcon class="w-[20px]" /></button>
+  <form class="flex items-center justify-center gap-3" @submit="submit">
+    <input
+      type="text"
+      name="city"
+      id="city"
+      class="rounded p-1"
+      placeholder="Riga..."
+      v-model="city"
+    />
+    <button type="submit"><SearchIcon class="w-[20px]" /></button>
   </form>
 </template>
